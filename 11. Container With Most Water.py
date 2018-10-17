8be8c962f6e1bc561fe4363136987bc414b16efc
simple5510 <simple5510@163.com>
2018-10-17 19:11:51 +0800

    add：+1

--- 11. Container With Most Water.py
+++ 11. Container With Most Water.py
@@ -3,3 +3,16 @@
 
 说明：你不能倾斜容器，且 n 的值至少为 2。
 """
+
+
+class Solution:
+    def maxArea(self, height):
+        i, j = 0, len(height) - 1
+        water = 0
+        while i < j:
+            water = max(water, (j - i) * min(height[i], height[j]))
+            if height[i] < height[j]:
+                i += 1
+            else:
+                j -= 1
+        return water
